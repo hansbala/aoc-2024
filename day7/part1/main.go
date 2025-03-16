0dcf1cbd6031aea9e38fe58d9ebf2952f47b3c99
Hans <hansbala@hansbala.com>
2025-03-15 21:50:47 -0700

    day 7!

---
+++ day7/part1/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type KeyWrapper struct {
+	goal  int64
+	index int
+}
+
+func main() {
+	cases := MustGetInput()
+	ans := int64(0)
+	for kw, perm := range cases {
+		goal := kw.goal
+		if IsValid(perm, goal) {
+			ans += goal
+		}
+	}
+	fmt.Printf("ans -> %d\n", ans)
+}
+
+func IsValid(perm []int64, goal int64) bool {
+	if len(perm) == 0 {
+		panic("received 0 in perm which should not happen")
+	}
+	if len(perm) == 1 {
+		if perm[0] == goal {
+			return true
+		}
+		return false
+	}
+	first, second := perm[0], perm[1]
+	// Case 1: Apply addition.
+	if IsValid(append([]int64{first + second}, perm[2:]...), goal) {
+		return true
+	}
+	// Case 2: Apply multiplication.
+	if IsValid(append([]int64{first * second}, perm[2:]...), goal) {
+		return true
+	}
+	// Doing either is invalid so return false.
+	return false
+}
+
+func MustGetInput() map[KeyWrapper][]int64 {
+	contents, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	r := map[KeyWrapper][]int64{}
+	for lineIdx, line := range strings.Split(string(contents), "\n") {
+		testAndNums := strings.Split(line, ": ")
+		if len(testAndNums) != 2 {
+			panic("expected two sections")
+		}
+		goal, err := strconv.Atoi(testAndNums[0])
+		if err != nil {
+			panic(fmt.Errorf("failed to parse goal: %w", err))
+		}
+		for _, numStr := range strings.Split(testAndNums[1], " ") {
+			currNum, err := strconv.Atoi(numStr)
+			if err != nil {
+				panic(fmt.Errorf("failed to parse number: %w", err))
+			}
+			keyWrapper := KeyWrapper{goal: int64(goal), index: lineIdx}
+			r[keyWrapper] = append(r[keyWrapper], int64(currNum))
+		}
+	}
+	return r
+}
